0b41f9b39376170f8950df9f70418fa4d72179f0
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-07-05 02:16:31 +0600

    creat component and header file and added google lexend fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl '>Recipe Calories</h1>
+      <Header></Header>
     </>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>Recipe Calories</h1>
+        </div>
+    );
+};
+
+export default Header;
